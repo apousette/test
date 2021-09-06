d85c7f477273b0702d0dad641c82b2ca20f415a3
unknown <adam.pousette@gmail.com>
2021-09-06 12:10:48 +0200

    skickat bla bla

---
+++ namn/namn/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace namn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej, vad heter du?");
+
+            string namn = Console.ReadLine();
+
+            Console.WriteLine("Hej " + namn + "!");
+        }
+    }
+}
---
+++ namn2/namn2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace namn2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej!");
+            Console.WriteLine("Vad är ditt förnamn?");  
+
+            string fnamn = Console.ReadLine();
+
+            Console.WriteLine("Vad är ditt efternamn?");
+
+            string enamn = Console.ReadLine();
+
+            Console.WriteLine("Hej " + fnamn + " " + enamn + "!");
+
+        }
+    }
+}
